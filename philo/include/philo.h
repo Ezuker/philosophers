d09c01d579e97aaeaaf78f41b7ab74cf36c2e378
bcarolle <bcarolle@student.42angouleme.fr>
2024-02-18 21:17:34 +0100

    makefile done

---
+++ philo/include/philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcarolle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/18 18:33:41 by bcarolle          #+#    #+#             */
+/*   Updated: 2024/02/18 18:34:26 by bcarolle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/time.h>
+
+# define EAT 0
+
+#endif
